99cba138fdfa7f3ce6c35d2621b53e36f60a2977
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-01-17 00:42:32 +0300

    added logger

--- example/example.coffee
+++ example/example.coffee
@@ -1,12 +1,13 @@
 koa = require 'koa'
 router = require 'koa-router'
+logger = require 'koa-logger'
 api = require '../lib/index'
 schema = require './schema'
 
 mongoUrl = process.env.MONGOLAB_URL
 
 app = koa()
-
+app.use logger()
 app.use router app
 api app, schema, mongoUrl
 
--- example/example.js
+++ example/example.js
@@ -1,11 +1,20 @@
+var api, app, koa, logger, mongoUrl, router, schema;
+
 koa = require('koa');
+
 router = require('koa-router');
+
+logger = require('koa-logger');
+
 api = require('../lib/index');
+
 schema = require('./schema');
 
 mongoUrl = process.env.MONGOLAB_URL;
 
 app = koa();
+
+app.use(logger());
 
 app.use(router(app));
 
